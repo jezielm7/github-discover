cbf68b87e13d80454564c203c943ae4a81ef9b39
jezielm7 <jezielmarques7@gmail.com>
2021-03-26 17:08:04 -0300

    search page: add functional feature & styled

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,14 +1,144 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Keyboard, Text } from 'react-native';
 
-import { Text } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import User from '../../types/User';
+import Repo from '../../types/Repo';
+
+import api from '../../services/api';
+
+import FilterIcon from 'react-native-vector-icons/Feather';
+
 import Header from '../../components/Header';
+import UserItem from '../../components/UserItem';
+import RepoItem from '../../components/RepoItem';
 
-import { Container } from './styles';
+import normalize from '../../utils/normalize';
+
+import {
+  Container,
+  FilterButton,
+  SearchForm,
+  Label,
+  Input,
+  MoreInfoButton,
+  MoreInfoText,
+  ScrollList,
+} from './styles';
 
 function Search() {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(true);
+
+  const [user, setUser] = useState<User>({} as User);
+  const [repositories, setRepositories] = useState<Repo[]>([]);
+
+  const [username, setUsername] = useState('');
+  const [usernameRepo, setUsernameRepo] = useState('');
+
+  const [favorites, setFavorites] = useState<number[]>([]);
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  };
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoredRepos = JSON.parse(response);
+        const favoredReposIds = favoredRepos.map((repo: Repo) => {
+          return repo.id;
+        })
+
+        setFavorites(favoredReposIds);
+      }
+    });
+  };
+
+  async function handleFiltersSubmit() {
+    loadFavorites();
+
+    if (usernameRepo) {
+      const repoResponse = await api.get(`/users/${usernameRepo}/repos`);
+
+      setUser({} as User);
+      setRepositories(repoResponse.data);
+      setUsernameRepo('');
+
+    } else if (username) {
+      const userResponse = await api.get(`/users/${username}`);
+
+      setUser(userResponse.data);
+      setRepositories([]);
+      setUsername('');
+    }
+
+    Keyboard.dismiss();
+    setIsFiltersVisible(!isFiltersVisible);
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavorites();
+    }, [])
+  );
+
   return (
     <Container>
-      <Header title="Pesquise por um usuário ou repositório" />
+      <Header
+        title="Pesquise por um usuário ou repositórios"
+        headerRight={(
+          <FilterButton onPress={handleToggleFiltersVisible}>
+            <FilterIcon name="filter" size={normalize(20)} color="#fff" />
+          </FilterButton>
+        )}
+      >
+        {isFiltersVisible && (
+          <SearchForm>
+            <Label>Pesquise um usuário</Label>
+            <Input
+              value={username}
+              onChangeText={text => setUsername(text)}
+              placeholder="Digite um usuário"
+              placeholderTextColor="#c1bccc"
+              autoCapitalize="none"
+            />
+
+            <Label>Listar repositórios de um usuário</Label>
+            <Input
+              value={usernameRepo}
+              onChangeText={text => setUsernameRepo(text)}
+              placeholder="Digite um usuário"
+              placeholderTextColor="#c1bccc"
+              autoCapitalize="none"
+            />
+
+            <MoreInfoButton onPress={handleFiltersSubmit}>
+              <MoreInfoText>Filtrar</MoreInfoText>
+            </MoreInfoButton>
+          </SearchForm>
+        )}
+      </Header>
+
+      <ScrollList
+        showsVerticalScrollIndicator={false}
+      >
+        {!!user.id ? (
+          <UserItem user={user} />
+        )
+          :
+          repositories.map((repo: Repo) => {
+            return (
+              <RepoItem
+                key={repo.id}
+                repo={repo}
+                favored={favorites.includes(repo.id)}
+              />
+            )
+          })
+        }
+      </ScrollList>
     </Container>
   );
 };
--- src/screens/Search/styles.ts
+++ src/screens/Search/styles.ts
@@ -1,5 +1,51 @@
 import styled from 'styled-components/native';
+import { RectButton, BorderlessButton } from 'react-native-gesture-handler';
+
+import normalize from '../../utils/normalize';
 
 export const Container = styled.View`
+  background-color: #f0f0f7;
+`;
 
+export const FilterButton = styled(BorderlessButton)``;
+
+export const SearchForm = styled.View`
+  margin-bottom: ${normalize(20)}px;
+`;
+
+export const Label = styled.Text`
+  color: #d4c2ff;
+`;
+
+export const Input = styled.TextInput`
+  margin-top: 4px;
+
+  height: ${normalize(54)}px;
+  padding: 0 ${normalize(16)}px;
+  border-radius: ${normalize(8)}px;
+  margin-bottom: ${normalize(16)}px;
+
+  color: #555;
+  background-color: #eee;
+  justify-content: center;
+`;
+
+export const MoreInfoButton = styled(RectButton)`
+  height: ${normalize(56)}px;
+  border-radius: ${normalize(8)}px;
+
+  align-items: center;
+  justify-content: center;
+  background-color: #3d148c;
+`;
+
+export const MoreInfoText = styled.Text`
+  color: #fff;
+  font-size: ${normalize(16)}px;
+`;
+
+
+export const ScrollList = styled.ScrollView`
+  margin-top: ${normalize(-40)}px;
+  padding: 0 ${normalize(16)}px ${normalize(16)}px;
 `;
