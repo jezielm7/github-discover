988b0f0d342c95f7ac1d6457ae0ce44177feada0
jezielm7 <jezielmarques7@gmail.com>
2021-03-26 17:06:21 -0300

    landing page: add responsiveness

--- src/screens/Landing/styles.ts
+++ src/screens/Landing/styles.ts
@@ -1,32 +1,34 @@
 import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
+import normalize from '../../utils/normalize';
+
 export const Container = styled.View`
   flex: 1;
-  padding: 40px;
   justify-content: center;
   background-color: #02060F;
+  padding: ${normalize(40)}px;
 `;
 
 
 export const ImageBackground = styled.ImageBackground`
   width: 102%;
-  padding: 25px 10px;
+  padding: ${normalize(25)}px ${normalize(10)}px;
 `;
 
 export const Header = styled.View`
-  top: 25px;
   align-items: center;
+  top: ${normalize(25)}px;
 `;
 
 export const Title = styled.Text`
   color: #fff;
-  font-size: 24px;
+  font-size: ${normalize(24)}px;
 `;
 
 export const ImageContainer = styled.View`
-  top: 15px;
   align-items: center;
+  top: ${normalize(15)}px;
 `;
 
 export const Image = styled.Image`
@@ -35,26 +37,29 @@ export const Image = styled.Image`
 
 export const ImageText = styled.Text`
   color: #fff;
-  bottom: 20px;
-  font-size: 22px;
+
+  bottom: ${normalize(20)}px;
+  font-size: ${normalize(22)}px;
 `;
 
 export const ButtonContainer = styled.View`
-  top: 32px;
   align-items: center;
+  top: ${normalize(32)}px; 
 `;
 
 export const Button = styled(RectButton)`
   width: 70%;
-  height: 65px;
-  border-radius: 10px;
+  height: ${normalize(65)}px;
+  border-radius: ${normalize(10)}px;
+  
   align-items: center;
   justify-content: center;
   background-color: #060e1f;
+  
 `;
 
 export const BtnText = styled.Text`
   color: #fff;
-  font-size: 20px;
   font-weight: bold;
+  font-size: ${normalize(20)}px;
 `;
